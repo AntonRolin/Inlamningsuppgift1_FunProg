411049bf3a5ab4706587a722981784ef5ff4d90e
Anton Rolin <antonrolin@hotmail.com>
2021-01-14 18:41:29 +0100

    Alla metoder färdiga

---
+++ src/Category.java
@@ -0,0 +1,11 @@
+/**
+ * Created by: Anton Rolin
+ * Date: 13/01/2021
+ * Time: 17:36
+ * Project: Inlamningsuppgift1_Lambdas
+ * Copyright: MIT
+ */
+public enum Category {
+    FOOD, HISTORY, CHEMISTRY
+
+}
---
+++ src/Question.java
@@ -0,0 +1,41 @@
+/**
+ * Created by: Anton Rolin
+ * Date: 13/01/2021
+ * Time: 17:38
+ * Project: Inlamningsuppgift1_Lambdas
+ * Copyright: MIT
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Question {
+    protected String question;
+    protected Category category;
+    protected List<String> answers = new ArrayList<>();
+    protected int correctAnswer;
+
+    public Question(String q, String a1, String a2, String a3, String a4, Category cat, int correct) {
+        question = q;
+        answers = Arrays.asList(a1, a2, a3, a4);
+        category = cat;
+        correctAnswer = correct;
+    }
+
+    public String getQuestionString(){
+        return question;
+    }
+
+    public Category getCategory(){
+        return category;
+    }
+
+    public int getCorrectAnswer(){
+        return correctAnswer;
+    }
+
+    public List<String> getAllAnswers(){
+        return answers;
+    }
+}
